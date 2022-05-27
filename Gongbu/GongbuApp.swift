5a9f8f31c94732a7e31232a9cd1b9d3a208ff52c
terry-koo <xpflxhfl94@naver.com>
2022-05-27 16:56:39 +0900

    first commit

---
+++ Gongbu/GongbuApp.swift
@@ -0,0 +1,17 @@
+//
+//  GongbuApp.swift
+//  Gongbu
+//
+//  Created by Terry Koo on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct GongbuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

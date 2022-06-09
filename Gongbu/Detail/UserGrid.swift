919dd0887b3480ef85bf49204b8e78df10a53bfb
rkddnr330 <rkddnr330@gmail.com>
2022-06-09 22:13:01 +0900

    [Style] UserGrid를 LazyVGrid로 구현

--- Gongbu/Detail/UserGrid.swift
+++ Gongbu/Detail/UserGrid.swift
@@ -7,101 +7,42 @@
 
 import SwiftUI
 
+//https://swiftobc.com/repo/Q-Mobile-QGrid-swift-table-view
+//Qgrid 라는 게 있는데, 자동으로 grid 만들어주는 라이브러리? 같은 거. 설치해서 사용하는 거
+
+//임의의 스터디 정보
+struct Study: Identifiable {
+    let id = UUID()
+    var user: [String] = ["a","b","c","d","e"]
+}
+
 struct UserGrid: View {
+    //LazyVGrid에서 3*3 형태의 Grid 쓰려면 3개의 GridItem을 columns에 추가
+    let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    @State private var study = Study()
+    
     var body: some View {
         VStack{
-        HStack {
-            Text("출결 현황")
-                .font(.title)
-                .bold()
-                .padding()
-            Spacer()
-        }
-        
-        HStack(alignment: .center) {
-            VStack(alignment: .center) {
-                Circle()
-                    .stroke(Color.pink.opacity(0.5), lineWidth: 3)
-                    .frame(width: 70, height: 70)
-                Text("이름")
+            HStack {
+                Text("출결 현황")
+                    .font(.title)
                     .bold()
+                    .padding()
+                Spacer()
             }
-            Spacer()
-            VStack {
-                Circle()
-                    .stroke(Color.pink.opacity(0.5), lineWidth: 3)
-                    .frame(width: 70, height: 70)
-                Text("이름")
-                    .bold()
-            }
-            Spacer()
-            VStack {
-                Circle()
-                    .stroke(Color.pink.opacity(0.5), lineWidth: 3)
-                    .frame(width: 70, height: 70)
-                Text("이름")
-                    .bold()
+            LazyVGrid(columns: columns) {
+                ForEach(0 ..< Int(study.user.count)) { value in
+                    VStack (alignment: .center) {
+                        Circle()
+                            .stroke(Color.pink.opacity(0.5), lineWidth: 3)
+                            .frame(width: 70, height: 70)
+                        Text(study.user[value])
+                            .bold()
+                    }
+                }
             }
         }
-        .padding(.horizontal)
-        
-        HStack(alignment: .center) {
-            VStack(alignment: .center) {
-                Circle()
-                    .stroke(Color.pink.opacity(0.5), lineWidth: 3)
-                    .frame(width: 70, height: 70)
-                Text("이름")
-                    .bold()
-            }
-            Spacer()
-            VStack {
-                Circle()
-                    .stroke(Color.pink.opacity(0.5), lineWidth: 3)
-                    .frame(width: 70, height: 70)
-                Text("이름")
-                    .bold()
-            }
-            Spacer()
-            VStack {
-                Circle()
-                    .stroke(Color.pink.opacity(0.5), lineWidth: 3)
-                    .frame(width: 70, height: 70)
-                Text("이름")
-                    .bold()
-            }
-        }
-        .padding(.horizontal)
-        
-        HStack(alignment: .center) {
-            VStack(alignment: .center) {
-                Circle()
-                    .stroke(Color.pink.opacity(0.5), lineWidth: 3)
-                    .frame(width: 70, height: 70)
-                Text("이름")
-                    .bold()
-            }
-            Spacer()
-            VStack {
-                Circle()
-                    .stroke(Color.pink.opacity(0.5), lineWidth: 3)
-                    .frame(width: 70, height: 70)
-                Text("이름")
-                    .bold()
-            }
-            Spacer()
-            VStack {
-                Circle()
-                    .stroke(Color.pink.opacity(0.5), lineWidth: 3)
-                    .frame(width: 70, height: 70)
-                Text("이름")
-                    .bold()
-            }
-        }
-        .padding(.horizontal)
-        
     }
-        .padding()
-}
 }
 
 struct UserGrid_Previews: PreviewProvider {

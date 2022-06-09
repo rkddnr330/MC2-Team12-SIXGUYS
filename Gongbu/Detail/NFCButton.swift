b3c4be0dd48f2c40de5bcd93291ba4c8db4d6016
rkddnr330 <rkddnr330@gmail.com>
2022-06-09 23:37:19 +0900

    [Style] NFC Button color code 수정

--- Gongbu/Detail/NFCButton.swift
+++ Gongbu/Detail/NFCButton.swift
@@ -18,11 +18,11 @@ struct NFCButton: View {
                     .font(.title)
                     .padding()
                     .foregroundColor(.white)
-                    .background(Color.ReadColor)
+                    .background(Color(red: 0.187, green: 0.417, blue: 0.533))
                     
             }
             .frame(width: 130, height: 76, alignment: .center)
-            .background(Color.ReadColor)
+            .background(Color(red: 0.187, green: 0.417, blue: 0.533))
             .cornerRadius(10)
             
             Button {
@@ -33,34 +33,14 @@ struct NFCButton: View {
                     .font(.title)
                     .padding()
                     .foregroundColor(.white)
-                    .background(Color.TagColor)
+                    .background(Color(red: 0.467, green: 0.696, blue: 0.821))
             }
             .frame(width: 130, height: 76, alignment: .center)
-            .background(Color.TagColor)
+            .background(Color(red: 0.467, green: 0.696, blue: 0.821))
             .cornerRadius(10)
         }
         .padding(.horizontal)
     }
-}
-
-extension Color {
-    static let ReadColor = Color(hex: "#3F6985")
-    static let TagColor = Color(hex: "84B0CE")
-}
-
-extension Color {
-  init(hex: String) {
-    let scanner = Scanner(string: hex)
-    _ = scanner.scanString("#")
-    
-    var rgb: UInt64 = 0
-    scanner.scanHexInt64(&rgb)
-    
-    let r = Double((rgb >> 16) & 0xFF) / 255.0
-    let g = Double((rgb >>  8) & 0xFF) / 255.0
-    let b = Double((rgb >>  0) & 0xFF) / 255.0
-    self.init(red: r, green: g, blue: b)
-  }
 }
 
 struct NFCButton_Previews: PreviewProvider {

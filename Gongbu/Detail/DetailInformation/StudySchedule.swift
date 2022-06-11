64c045de80a23e0d4003682fd5dc26879786be7c
rkddnr330 <rkddnr330@gmail.com>
2022-06-11 23:59:08 +0900

    [Feat] 일정 삭제 기능 대신 편집 기능 추가

--- Gongbu/Detail/DetailInformation/StudySchedule.swift
+++ Gongbu/Detail/DetailInformation/StudySchedule.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct StudySchedule: View {
     @State private var shedule = ["월요일 PM 5:00", "수요일 PM 5:00", "금요일 AM 10:00"]
+    @State private var isShowing = false
     
     var body: some View {
         VStack(alignment: .leading){
@@ -19,26 +20,30 @@ struct StudySchedule: View {
                     .bold()
                 Spacer()
                 Button {
+                    isShowing = true
                     print("일정 추가")
                 } label: {
-                    Image(systemName: "plus.square")
-                        .font(.title)
+                    Text("EDIT")
+                        .padding(4)
+                        .font(.subheadline)
                         .foregroundColor(.black)
+                        .border(.black, width: 2)
+                }
+                .sheet(isPresented: $isShowing) {
+                    Text("일정 편집 (일정 추가, 삭제)")
+                    Button {
+                        isShowing = false
+                    } label: {
+                        Text("DONE")
+                    }
+                    .padding()
                 }
             }
             .padding(.vertical)
+            
             VStack(alignment: .leading){
-                List {
-                    ForEach(shedule, id: \.self) { index in
-                        Text(index)
-                    }
-//                    .onDelete(perform: delete)
-                    .onDelete { indexSet in
-                        withAnimation {
-                            shedule.remove(atOffsets: indexSet)
-                        }
-                    }
-                    .listStyle(.plain)
+                ForEach(shedule, id: \.self) { index in
+                    Text(index)
                 }
             }
             .font(.title2)
@@ -46,7 +51,7 @@ struct StudySchedule: View {
             .foregroundColor(.black)
         }
         .padding()
-        .frame(width: 320, height: 400)
+        .frame(width: 320, height: 200)
         .background(Color(red: 0.969, green: 0.969, blue: 0.969))
         .cornerRadius(10)
     }

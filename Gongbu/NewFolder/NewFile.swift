26770c20c53916983fc45817fab963b37f44e761
terry-koo <xpflxhfl94@naver.com>
2022-06-12 17:09:42 +0900

    add NewFolder and NewFile

---
+++ Gongbu/NewFolder/NewFile.swift
@@ -0,0 +1,20 @@
+//
+//  NewFile.swift
+//  Gongbu
+//
+//  Created by Terry Koo on 2022/06/12.
+//
+
+import SwiftUI
+
+struct NewFile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewFile_Previews: PreviewProvider {
+    static var previews: some View {
+        NewFile()
+    }
+}

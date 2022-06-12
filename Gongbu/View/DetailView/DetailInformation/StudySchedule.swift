2d746b1aa681f85f2abd7d9c134d68016005686a
rkddnr330 <rkddnr330@gmail.com>
2022-06-12 18:17:00 +0900

    [Feat] DetailView K  작업목록

---
+++ Gongbu/View/DetailView/DetailInformation/StudySchedule.swift
@@ -0,0 +1,64 @@
+//
+//  StudySchedule.swift
+//  Gongbu
+//
+//  Created by Park Kangwook on 2022/06/12.
+//
+
+import SwiftUI
+
+struct StudySchedule: View {
+    @State private var schedule = ["월요일 PM 5:00", "수요일 PM 5:00", "금요일 AM 10:00"]
+    @State private var isShowingEdit = false
+    
+    var body: some View {
+        VStack(alignment: .leading){
+            HStack {
+                Text("일정")
+                    .font(.title)
+                    .foregroundColor(.black)
+                    .bold()
+                Spacer()
+                Button {
+                    isShowingEdit = true
+                    print("일정 추가")
+                } label: {
+                    Text("EDIT")
+                        .padding(4)
+                        .font(.subheadline)
+                        .foregroundColor(.black)
+                        .border(.black, width: 2)
+                }
+                .sheet(isPresented: $isShowingEdit) {
+                    Text("일정 편집 (일정 추가, 삭제)")
+                    Button {
+                        isShowingEdit = false
+                    } label: {
+                        Text("DONE")
+                    }
+                    .padding()
+                }
+            }
+            .padding(.vertical)
+            
+            VStack(alignment: .leading){
+                ForEach(schedule, id: \.self) { each in
+                    Text(each)
+                }
+            }
+            .font(.title2)
+            .padding(.vertical)
+            .foregroundColor(.black)
+        }
+        .padding()
+        .frame(width: 320, height: 200)
+        .background(Color(red: 0.969, green: 0.969, blue: 0.969))
+        .cornerRadius(10)
+    }
+}
+
+struct StudySchedule_Previews: PreviewProvider {
+    static var previews: some View {
+        StudySchedule()
+    }
+}
---
+++ Gongbu/View/DetailView/DetailNFC/NFCButton.swift
@@ -0,0 +1,50 @@
+//
+//  NFCButton.swift
+//  Gongbu
+//
+//  Created by Park Kangwook on 2022/06/12.
+//
+
+import SwiftUI
+
+struct NFCButton: View {
+    var body: some View {
+        HStack(spacing:50){
+            Button {
+               print("Read 발동")
+            } label: {
+                Text("Read")
+                    .bold()
+                    .font(.title)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color(red: 0.187, green: 0.417, blue: 0.533))
+                    
+            }
+            .frame(width: 130, height: 76, alignment: .center)
+            .background(Color(red: 0.187, green: 0.417, blue: 0.533))
+            .cornerRadius(10)
+            
+            Button {
+                print("Tag 발동")
+            } label: {
+                Text("Tag")
+                    .bold()
+                    .font(.title)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color(red: 0.467, green: 0.696, blue: 0.821))
+            }
+            .frame(width: 130, height: 76, alignment: .center)
+            .background(Color(red: 0.467, green: 0.696, blue: 0.821))
+            .cornerRadius(10)
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct NFCButton_Previews: PreviewProvider {
+    static var previews: some View {
+        NFCButton()
+    }
+}

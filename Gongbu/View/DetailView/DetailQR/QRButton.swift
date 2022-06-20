5a4c5a5b0969434192d3106bc0cdc4b436fdf5b7
Jinsan Kim <gkjs1129@gmail.com>
2022-06-20 14:44:14 +0900

    [Feat] 차트 업데이트 기능 추가

--- Gongbu/View/DetailView/DetailQR/QRButton.swift
+++ Gongbu/View/DetailView/DetailQR/QRButton.swift
@@ -20,21 +20,21 @@ struct QRButton: View {
         
         let userID:String = Auth.auth().currentUser!.uid
         
-        HStack(spacing:50){
+        HStack(spacing:20){
             Button {
                 print("Read 발동")
                 self.isPresentingScanner = true
             } label: {
-                Text("Scan")
+                Text("QR 읽기")
                     .bold()
-                    .font(.title)
+                    .font(.body)
                     .padding()
                     .foregroundColor(.white)
-                    .background(Color(red: 0.187, green: 0.417, blue: 0.533))
+                    .background(Color(red: 0.467, green: 0.696, blue: 0.821))
                     
             }
-            .frame(width: 130, height: 76, alignment: .center)
-            .background(Color(red: 0.187, green: 0.417, blue: 0.533))
+            .frame(width: 100, height: 76, alignment: .center)
+            .background(Color(red: 0.467, green: 0.696, blue: 0.821))
             .cornerRadius(10)
             .sheet(isPresented: $isPresentingScanner) {
                 QRScannerView(isPresentingScanner: $isPresentingScanner, roomId: roomId)
@@ -72,7 +72,7 @@ struct QRButton: View {
                     //--------------
                     // 지각
                     //--------------
-                    else if result < 0 && result >= -10 {
+                    else if result < 0 && result >= -1400 {
                         ref.updateData([
                             "currentAtt.\(userID)" : 1
                         ])
@@ -80,19 +80,35 @@ struct QRButton: View {
                 }
                 
             } label: {
-                Text("Tag")
+                Text("QR 생성")
                     .bold()
-                    .font(.title)
+                    .font(.body)
                     .padding()
                     .foregroundColor(.white)
                     .background(Color(red: 0.467, green: 0.696, blue: 0.821))
             }
-            .frame(width: 130, height: 76, alignment: .center)
+            .frame(width: 100, height: 76, alignment: .center)
             .background(Color(red: 0.467, green: 0.696, blue: 0.821))
             .cornerRadius(10)
             .sheet(isPresented: $showQRCode) {
                 QRGenerateView(roomId: roomId)
             }
+            ///K 수정
+            Button(action: {
+                updateScore()
+                clearData()
+            }, label: {
+                Text("출결 마감")
+                    .bold()
+                    .font(.body)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color(red: 0.187, green: 0.417, blue: 0.533))
+            })
+            .frame(width: 100, height: 76, alignment: .center)
+            .background(Color(red: 0.187, green: 0.417, blue: 0.533))
+            .cornerRadius(10)
+            ///
         }
         .padding(.horizontal)
         .onAppear {
@@ -112,10 +128,96 @@ struct QRButton: View {
         let min = Int(arr[1])!
         return hour + min + (60 * calAmpm)
     }
+    
+    ///K수정
+    func clearData() {
+        let myUid = Auth.auth().currentUser!.uid
+        let db = Firestore.firestore()
+        let ref = db.collection("Study1").document(roomId)
+        
+        // ========================
+        // 실시간 변화 확인
+        // ========================
+        ref.getDocument(){ result, error in
+            guard let data = result!.data()!["currentAtt"] else {
+                print("Document data was empty")
+                return
+            }
+            let asdf = data as! Dictionary<String, Int>
+            for key in asdf.keys{
+                ref.updateData([
+                    "currentAtt.\(key)": 0
+                ])
+            }
+            print("Current data: \(asdf)")
+            print(type(of: asdf))
+            
+        }
+    }
+
+    func updateScore() {
+        // let myUid = Auth.auth().currentUser!.uid
+        let db = Firestore.firestore()
+        // document는 룸 아이디를 받아와야한다.
+        let ref = db.collection("Study1").document(roomId)
+        
+        
+        // ========================
+        // 실시간 변화 확인
+        // ========================
+        ref.getDocument(){ result, error in
+            guard let data = result!.data()!["currentAtt"] else {
+                print("Document data was empty")
+                return
+            }
+            guard let findId = result!.data()!["memberId"] else {
+                print("Document data was empty")
+                return
+            }
+            
+            
+            let asdf = data as! Dictionary<String, Int>
+            for (key, value) in asdf {
+                let arr = findId as! [String]
+                print(arr)
+                // 유저 아이디를 가지고 있는 memberID의 인덱스
+                let idx:Int = arr.firstIndex(of: "\(key)")!
+                print("\(key) \(idx)")
+                
+                // 받아온 인덱스에 스코어 만큼 더해주면 됨
+                
+                if value == 5 {
+                    ref.updateData([
+                        "numberOfAttendance.\(idx)": FieldValue.increment(Int64(1))
+                    ])
+                }
+
+                else if value == 1 {
+                    ref.updateData([
+                        "numberOfLate.\(idx)": FieldValue.increment(Int64(1))
+                    ])
+                }
+
+                else if value == 0 {
+                    ref.updateData([
+                        "numberOfAbsent.\(idx)": FieldValue.increment(Int64(1))
+                    ])
+                }
+                
+                ref.updateData([
+                    "attendancePoint.\(idx)": FieldValue.increment(Int64(value))
+                ])
+            }
+            print("Current data: \(asdf)")
+            print(type(of: asdf))
+            
+        }
+    }
+    ///
 }
 
 //struct QRButton_Previews: PreviewProvider {
 //    static var previews: some View {
-//        QRButton()
+//        QRButton(roomId: roomId)
 //    }
 //}
